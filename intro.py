6285b94a41427a0198f4e8f2aae59bf9d5264fcc
LivinkumarStackly <livinkumar@thestackly.com>
2026-04-29 11:11:35 +0530

    Intro

---
+++ intro.py
@@ -0,0 +1,17 @@
+# print(99)
+# these line are using print function
+
+print(9)
+print("Hello")
+print(9)
+print("Hello")
+print(9)
+
+
+print("Hello")
+print(9)
+print("Hello")
+print(9)
+print("Hello")
+print(9)
+print("Hello")
