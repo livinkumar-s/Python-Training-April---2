7e2f2d1533176fe64a6b18e15a21bc4d31c8d0e8
LivinkumarStackly <livinkumar@thestackly.com>
2026-05-04 11:16:08 +0530

    Variables, Datatypes and Type casting

---
+++ VarDTTypeCasting.py
@@ -0,0 +1,38 @@
+# a=10
+
+# Print=10
+# print(apple)
+
+# full_name="Livinkumar"
+# print(full_name)
+
+# a=10
+# a=2
+# print(a)
+# a=-4
+
+# str1="I'm leo"
+# str2='I just said "roger"'
+# str3=I'm leo. I said "Roger"
+
+# print(str1)
+# print(str2)
+
+# a="Hello"+" "+"world"
+# print(a)
+
+# print(6+6) #add
+# print("6"+"6") #concatination
+
+# print(6+"6")
+
+# name="Vijay"
+# age=53
+# print("His name is "+name)
+# print("his name is " + name + ". He is " + str(age) + " years old.")
+# his name is Vijay. He is 53 years old.
+
+# age=int(input("Enter your age: ")) #"155" -> 155
+# print(age+100)
+# print(age-100)
+
