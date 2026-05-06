1a5ac33a09236dbd7ca0b2a65ac20034695d4b10
LivinkumarStackly <livinkumar@thestackly.com>
2026-05-06 11:32:03 +0530

    Operators(5 main type)

--- Operators.py
+++ Operators.py
@@ -1,5 +1,5 @@
-a=45
-b=6
+# a=45
+# b=6
 # print(a+b)
 # print(a-b)
 # print(a*b)
@@ -7,4 +7,53 @@ b=6
 # print(a//b)
 # print(3%5) 
 # print(3**10)
+
+# print(3+4-6*8/4)
+
+# Comparison
+# a=100
+# b=100
+
+# print(a==b)
+# print(a!=b)
+# print(a<b)
+# print(a+2>b)
+# print(a<=b)
+# print(a>=b)
+# print(-4<-2)
+
+# Logical
+# a=15 
+# b=12 
+# print(a and b)
+# print(a or b)
+# print(not b)
+
+# print((a<b) and (a>b and a**2<b*10 or not (a**2==b-1**2)))
+
+# Assignment Operators 
+
+# x=10
+# x=20
+
+# x+=5 # x=x+5
+# x-=10
+# x*=10
+# x/=30
+# x//=3
+# x%=10
+# x**=2
+
+# print(x)
+
+
+# Bitwise 
+
+# print(13&44)
+# print(13|44)
+print(13^44)
+# print(~-44)
+
+# print(99>>4)
+# print(99<<2)
 
