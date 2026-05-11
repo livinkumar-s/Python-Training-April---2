eb6dc83db65362cb3b5b7755a3a865708ca2db96
LivinkumarStackly <livinkumar@thestackly.com>
2026-05-11 11:54:59 +0530

    Control Flow - Conditional Clause

---
+++ ControlFlow.py
@@ -0,0 +1,86 @@
+# print(12)
+# print(13)
+# print(14)
+
+# age=int(input("Enter Your age: "))
+# if age>=18:
+#     print("Can Vote")
+# else:
+#     print("Cannot Vote")
+
+# a=10
+
+# if a>10:
+#     print("Step1")
+#     print("Step2")
+#     print("Step3")
+# elif a<10:
+#     print("From Elif 1 block")
+# elif a>10:
+#     print("From Elif 2 block")
+# elif a!=10:
+#     print("From Elif 3 block")
+# else:
+#     print("From else Block...!")
+
+# print("Step4")
+
+# mark = int(input("Enter your mark: "))
+ 
+# if mark>=90 and mark<=100:
+#     print("Grade: O")
+ 
+# elif mark>=18 and mark<=89:  #80 <= mark <= 89
+#     print("Grade: A+")
+ 
+# elif 70 <= mark <= 79:
+#     print("Grade: A")
+ 
+# elif 60 <= mark <= 69:
+#     print("Grade: B")
+ 
+# elif 50 <= mark <= 59:
+#     print("Grade: C")
+ 
+# elif 0<=mark < 50:
+#     print("Grade: Fail")
+ 
+# else:
+#     print("Invalid Marks")
+
+# x=90
+
+# if x>80:
+#     if x<=90:
+#         print("Hello")
+
+# a=int(input("Enter Your Number: "))
+
+# if a>10:
+#     if a>15:
+#         print("Very large!")
+#     else:
+#         print("Large")
+# else:
+#     if a<5:
+#         print("Very Small")
+#     else:
+#         print("Small")
+
+accType="Current"
+balance=10000000
+
+# saving 1000<=
+
+# if accType=="Savings":
+#     if balance>=1000:
+#         print("Eligible")
+#     else:
+#         print("Not Eligible")
+# else:
+#     print("Not Eligible")
+
+if accType=="Savings" and balance>=1000:
+    print("Eligible")
+else:
+    print("Not Eligible")
