0576ca03e4f81d3a0400cc219b52aabfda8f5b99
LivinkumarStackly <livinkumar@thestackly.com>
2026-05-13 11:52:21 +0530

    Loops - While and For

---
+++ Loop.py
@@ -0,0 +1,105 @@
+# i=0
+
+# while i<5:
+#     print(10)
+#     print(11)
+#     print(12)
+#     print("--------------")
+#     i-=1
+
+# No of execution --> infinite
+# i --> 0,-1,-2
+
+# a="Hellosioufiowrgkajdghfiowshdifguh"
+
+# print(a[-2])
+
+# For Loop
+
+# a="Viratkohli"
+# a=range(4)
+
+
+# print(len(a))
+
+# for i in range(2,20,3):
+#     print(i)
+#     print("--------------")
+
+# No of execution --> 11
+
+# i = 1
+# while i <= 100:
+#    print(i)
+#    i += 1
+ 
+ 
+# for i in range(1, 101):
+#     print(i)
+ 
+
+# i = 100
+ 
+# while i >= 1:
+#     print(i)
+#     i -= 1
+ 
+ 
+# for i in range(100, 0, -1):
+#     print(i)
+
+
+# i = 1
+# while i <= 100:
+#    if i<=50:
+#       break
+#    print(i)
+#    i += 1
+
+# numbers --> 1,2......50
+# i --> 1, 2.... 50
+
+# i great that 100
+# i value goes greater than 50
+
+# for i in range(1,101):
+#     print(i)
+#     print("-------")
+#     if i==50:
+#         continue
+
+# i = 1
+# while i <= 100:
+#    if i==50:
+#       continue
+#    print(i)
+#    i += 1
+
+# numbers --> 1,49
+# i ---> 50, 50, 50...
+# no of execution --> infinite 
+
+# i > 100
+
+
+# secret = 33
+
+# while True:
+#     num = int(input("Enter Your Number: "))
+#     if secret==num:
+#         print("You won...!")
+#         break
+#     else:
+#         print("Try Again")
+
+
+# 10, 100,1000
+
+# when user finds secret
+
+# Nested Loop 
+
+for i in range(1,6): 
+    for j in range(1,6):
+        print(str(i)+","+str(j))
+
