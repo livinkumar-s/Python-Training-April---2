1591a4ef9ca6aec15ed510efaf3da3674db5535d
LivinkumarStackly <livinkumar@thestackly.com>
2026-05-15 11:14:42 +0530

    Datastructures

---
+++ Datastructures.py
@@ -0,0 +1,60 @@
+# a="Hello guys"
+
+# print(a[-2:])
+# print(a[:7])
+# print(a[2:7])
+# print(a[6:])
+# print(a[:5])
+# print(a[::-1])
+
+# List 
+# lis1= [
+# 1,
+# 2,
+# 3,
+# 2.2,
+# 3.3,
+# "Hi",
+# "Hello",
+# True,
+# False,
+# 2,2
+# ]
+# print(len(lis1))
+# print(lis1[-4])
+# print(lis1[5:6]) #[]
+
+# lis1[2]=33
+# print(lis1)
+
+
+# newLis=[]
+
+# for i in lis2:
+#     if i not in newLis:
+#         newLis.append(i)
+
+# print(newLis)
+
+
+# lis2.append(99)
+# lis2.append(999)
+
+# lis2.insert(5,0)
+# lis2.extend([-1,-2,-3])
+
+# lis2.append("Hello")
+# lis2.remove(2)
+# lis2.pop(2)
+# lis2.pop()
+lis2= [234,234,24,2,2]
+
+# lis2.pop()
+# print(lis2.index(5))
+# print(lis2.index(334))
+
+# lis2.sort(reverse=1)
+
+# print(lis2[::-1])
+
+# print("A" not in "Apple")
