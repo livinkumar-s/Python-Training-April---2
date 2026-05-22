fb88a7dbcca0831f1662566c35d22aadc805a61b
LivinkumarStackly <livinkumar@thestackly.com>
2026-05-22 11:17:12 +0530

    String

---
+++ String.py
@@ -0,0 +1,37 @@
+# str1="hello"
+# print(str1*3)
+
+# name="Vijay"
+# age=53
+# print("His name is "+name)
+# print("his name is " + name + ". He is " + str(age) + " years old.")
+# print(f"his name is {name}. He is {age} years old.")
+
+str1="I would love to code"
+
+# print(str1.upper())
+# print(str1.lower())
+# print(str1.title())
+# print(str1.capitalize())
+
+# print(str1.strip())
+# print(str1.lstrip())
+# print(str1.rstrip())
+
+# lis=str1.split()
+# print(lis)
+
+# lis1=['I', 'would', 'love', 'to', 'code']
+# str1="--".join(lis1)
+# print(str1)
+
+# name="Josephvijay"
+
+# print(name.find("z"))
+# print(name.count("a"))
+
+# print(name.startswith("Jo"))
+# print(name.endswith("Jo"))
+# print("123a".isdigit())
+# print(name.isalpha())
+
